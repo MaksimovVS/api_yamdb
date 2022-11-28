a24daf65dad192a0442058b917eee04dd45d13c0
Vladimir Maksimov <maksimovv0ff@yandex.ru>
2022-11-29 01:03:25 +0300

    Создано и зарегистрировано приложение users. Описана модель User, выполнены миграции.

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    ADMIN = 1
+    MODERATOR = 2
+    USER = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (MODERATOR, 'Moderator'),
+        (USER, 'User'),
+    )
+
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICES,
+        blank=True,
+        null=True,
+    )
+
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+        null=True,
+    )
