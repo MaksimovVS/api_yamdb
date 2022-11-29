c3434aa1f8cb4a5d93bfbb57b6f9f2a9d116231e
Kwuh <vadimseliverstovkw@gmail.com>
2022-11-29 13:21:33 +0300

    Category create

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,25 @@
 # reviews/models.py
 
+import datetime as dt
+
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-pass
+from users.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(
+        "Название категории",
+        max_length=200
+    )
+    slug = models.SlugField(
+        "slug",
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+
+    def __str__(self):
+        return self.name
